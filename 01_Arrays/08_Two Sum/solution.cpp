bbf1a37171a659836f8707868262072e7f11e11f
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-09 11:32:55 +0530

    Update solution.cpp

--- 01_Arrays/08_Two Sum/solution.cpp
+++ 01_Arrays/08_Two Sum/solution.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            int r=target-nums[i];
+            if(m.find(r) !=m.end()){
+                ans.push_back(m[r]);
+                ans.push_back(i);
+            }
+            m[nums[i]]=i;
+
+        }
+        return ans;
+            
+    }
+};
 
