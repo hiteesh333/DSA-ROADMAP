91933a4128af5c3d180907f108fda6195dfbc3ec
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-09 11:33:36 +0530

    Update solution.cpp

--- 01_Arrays/09_Longest Common Prefix/solution.cpp
+++ 01_Arrays/09_Longest Common Prefix/solution.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        sort(strs.begin(),strs.end());
+        string start=strs[0],end=strs[strs.size()-1];
+        int i=0;
+        while(i<start.size() && start[i]==end[i]){
+            i++;
+        }
+        return start.substr(0,i);
 
+    }
+};
