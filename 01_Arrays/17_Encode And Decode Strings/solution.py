ddebcdbe4892f11e35e06c5ce83c74ba595ee14f
jaswanthgodavarthi <jaswanthgodavarthy@gmail.com>
2025-11-24 21:50:42 +0530

    adding 4 codes in root branch

--- 01_Arrays/17_Encode And Decode Strings/solution.py
+++ 01_Arrays/17_Encode And Decode Strings/solution.py
@@ -1,1 +1,20 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+        for w in strs:
+            s += str(len(w)) + "#" + w
+        return s
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            j += 1
+            word = s[j : j + length]
+            res.append(word)
+            i = j + length
+        return res
 
