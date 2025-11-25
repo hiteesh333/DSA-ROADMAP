fc5a32bdc09199bb137b652a4f4dcae0f053a27a
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-25 10:31:59 +0530

    Update solution.cpp

--- 01_Arrays/13_Design Hashset/solution.cpp
+++ 01_Arrays/13_Design Hashset/solution.cpp
@@ -1,1 +1,20 @@
+class MyHashSet {
+public:
+    MyHashSet() {
+       
+    }
+    set<int>s;
+    void add(int key) {
+        s.insert(key);
+    }
+    
+    void remove(int key) {
+        s.erase(key);
+    }
+    
+    bool contains(int key) {
+        if(s.find(key) != s.end()) return true;
+        else return false;
+    }
+};
 
