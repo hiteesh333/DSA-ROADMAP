0fb9c1b1626de42f6249c28320e6feeea959d627
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-25 10:38:32 +0530

    Update solution.py

--- 01_Arrays/20_Valid Sudoku/solution.py
+++ 01_Arrays/20_Valid Sudoku/solution.py
@@ -1,8 +1,1 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        suma = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[suma] = nums[i]
-                suma += 1
-        return suma
+
