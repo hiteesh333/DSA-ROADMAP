654e79b56ba9c17a4c9c4013f2f8737c44216aac
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-26 10:28:20 +0530

    Update solution.cpp

--- 01_Arrays/17_Encode And Decode Strings/solution.cpp
+++ 01_Arrays/17_Encode And Decode Strings/solution.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded="";
+        for(string &s:strs){
+            encoded +=to_string(s.size())+'#'+s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>ans;
+        int i=0;
+        while(i<s.size()){
+            int index=s.find('#',i);
+            int len=stoi(s.substr(i,index-i));
+            ans.push_back(s.substr(index+1,len));
+            i=index+len+1;
+        }
+        return ans;
+    }
+};
 
