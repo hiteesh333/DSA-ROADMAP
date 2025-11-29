686fd4d99428c43d509e27527b2650dc1116047a
hiteesh333 <129778341+hiteesh333@users.noreply.github.com>
2025-11-29 11:14:21 +0530

    Update solution.cpp

--- 01_Arrays/26_Find Pivot Index/solution.cpp
+++ 01_Arrays/26_Find Pivot Index/solution.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int ts=0;
+        int ls=0;
+        for(int num:nums){
+            ts +=num;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(ls == ts-ls-nums[i]) return i; //rs=ts-ls-nums[i]
+            ls +=nums[i];
+        }
+        return -1;
+    }
+};
